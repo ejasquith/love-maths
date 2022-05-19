eed4285c00c4e7d1a5e15e7d1a85ab32289a1072
ejasquith <emily.asquith@icloud.com>
2022-05-19 23:27:12 +0000

    Make division game provide integer answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -131,7 +131,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand1').textContent = operand1 * operand2;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = '/';
 }
