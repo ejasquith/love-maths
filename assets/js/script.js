4062c005f0881d16b717d0185068263e286a0cba
ejasquith <emily.asquith@icloud.com>
2022-05-19 22:36:35 +0000

    Add functionality to display addition question

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,7 +58,9 @@ function incrementWrongAnswer() {
 }
 
 function displayAdditionQuestion(operand1, operand2) {
-
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
 }
 
 function displaySubtractQuestion(operand1, operand2) {
