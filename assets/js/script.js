4a304f492bcf3ca455649277265d030be8e833ba
ejasquith <emily.asquith@icloud.com>
2022-05-19 22:46:53 +0000

    Implement calculateCorrectAnswer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,8 +45,29 @@ function checkAnswer() {
 
 }
 
+/**
+ * Gets the operands and operator directly from the DOM
+ * and returns the correct answer.
+ */
 function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
 
+    if (operator === '+') {
+        return [operand1 + operand2, 'addition'];
+    } else if (operator === '-') {
+        return [operand1 - operand2, 'subtract'];
+    } else if (operator === 'x') {
+        return [operand1 * operand2, 'multiply'];
+    } else if (operator === '/') {
+        return [operand1 / operand2, 'division'];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting`;
+    }
+
+    return answer;
 }
 
 function incrementScore() {
