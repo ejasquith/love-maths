0dd5a7471875d1e88fb9c91ec73a584291e0981a
ejasquith <emily.asquith@icloud.com>
2022-05-19 23:12:40 +0000

    Prevent subtract game from having negative answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -110,8 +110,13 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    if (operand1 >= operand2) {
+        document.getElementById('operand1').textContent = operand1;
+        document.getElementById('operand2').textContent = operand2;
+    } else {
+        document.getElementById('operand1').textContent = operand2;
+        document.getElementById('operand2').textContent = operand1;
+    }
     document.getElementById('operator').textContent = '-';
 }
 
