1017bcb054e1787fcd7e836cd7a5d2d2ca27671b
ejasquith <emily.asquith@icloud.com>
2022-05-19 23:08:26 +0000

    Implement displayQuestion functions for other operators

--- assets/js/script.js
+++ assets/js/script.js
@@ -110,13 +110,19 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion(operand1, operand2) {
-
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '-';
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
-
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = 'x';
 }
 
 function displayDivisionQuestion(operand1, operand2) {
-
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '/';
 }
